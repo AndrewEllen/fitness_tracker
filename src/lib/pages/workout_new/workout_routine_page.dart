d4a837a34afbe2d47a43e14d6a7d97415f62df9f
ANDREW ELLEN <18004410@uhi.ac.uk>
2024-01-04 01:35:05 +0000

    Clicking routine opens routines page

---
+++ src/lib/pages/workout_new/workout_routine_page.dart
@@ -0,0 +1,14 @@
+import 'package:fitness_tracker/constants.dart';
+import 'package:flutter/material.dart';
+
+class WorkoutRoutinePage extends StatelessWidget {
+  const WorkoutRoutinePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Routines",
+      style: boldTextStyle,
+    );
+  }
+}
--- src/lib/widgets/workout_new/home_page_routines_list.dart
+++ src/lib/widgets/workout_new/home_page_routines_list.dart
@@ -1,7 +1,11 @@
 import 'package:fitness_tracker/constants.dart';
+import 'package:fitness_tracker/exports.dart';
 import 'package:fitness_tracker/widgets/workout_new/workout_bottom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+import '../../pages/workout_new/workout_routine_page.dart';
 
 class HomePageRoutinesList extends StatefulWidget {
   const HomePageRoutinesList({Key? key}) : super(key: key);
@@ -11,6 +15,7 @@ class HomePageRoutinesList extends StatefulWidget {
 }
 
 class _HomePageRoutinesListState extends State<HomePageRoutinesList> {
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -21,7 +26,7 @@ class _HomePageRoutinesListState extends State<HomePageRoutinesList> {
         return Padding(
           padding: EdgeInsets.only(bottom: 10.h),
           child: InkWell(
-            onTap: () {},
+            onTap: () => context.read<PageChange>().changePageCache(WorkoutRoutinePage()),
             child: Column(
               children: [
                 ListTile(
@@ -59,7 +64,7 @@ class _HomePageRoutinesListState extends State<HomePageRoutinesList> {
                   ),
                 ),
                 InkWell(
-                  onTap: () {},
+                  onTap: () => context.read<PageChange>().changePageCache(WorkoutRoutinePage()),
                   child: Ink(
                     decoration: const BoxDecoration(
                       border: Border(
