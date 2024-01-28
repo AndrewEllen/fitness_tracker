640f49984e508e483467a6fe4237180df163e299
Andrew Ellen <18004410@uhi.ac.uk>
2024-01-28 01:42:11 +0000

    Added video background

--- src/lib/pages/general_new/auth_landing_page.dart
+++ src/lib/pages/general_new/auth_landing_page.dart
@@ -1,24 +1,39 @@
+import 'dart:ui';
+
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitness_tracker/pages/general_new/user_login_page.dart';
 import 'package:fitness_tracker/pages/general_new/user_signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sign_in_button/sign_in_button.dart';
+import 'package:video_player/video_player.dart';
 import '../../constants.dart';
+import '../../widgets/general/faded_widget.dart';
 
 void main() {
   runApp(LandingPage());
 }
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   LandingPage({super.key});
 
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
   final emailController = TextEditingController();
+
   final userNameController = TextEditingController();
+
   final passwordController = TextEditingController();
 
   final GlobalKey<FormState> emailKey = GlobalKey<FormState>();
+
   final GlobalKey<FormState> userNameKey = GlobalKey<FormState>();
+
   final GlobalKey<FormState> passwordKey = GlobalKey<FormState>();
 
   Future<void> signInWithTwitter() async {
@@ -40,178 +55,249 @@ class LandingPage extends StatelessWidget {
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
+  late VideoPlayerController videoController;
+
+  @override
+  void initState() {
+
+
+    videoController = VideoPlayerController.asset("assets/FITBackgroundVideo.mp4")
+      ..initialize().then((_) {
+
+        videoController.play();
+        videoController.setLooping(true);
+
+        setState(() {});
+      });
+
+
+    super.initState();
+  }
+
+
+  @override
+  void dispose() {
+    videoController.dispose();
+    super.dispose();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        //color: appPrimaryColour.withOpacity(0.4),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: SingleChildScrollView(
-                physics: const NeverScrollableScrollPhysics(),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const SizedBox(height: 80.0),
-                    //Image.asset(
-                    //  'assets/logo/applogo.png',
-                    //  height: 80.0,
-                    //),
-                    const SizedBox(height: 40.0),
+      body: Stack(
+        children: [
 
-                    Center(
-                      child: Column(
-                        children: [
+          SizedBox.expand(
 
-                          Row(
+            child: FittedBox(
+
+              fit: BoxFit.fill,
+              child: SizedBox(
+
+                width: videoController.value.size?.width ?? 0,
+                height: videoController.value.size?.height ?? 0,
+                child: VideoPlayer(videoController),
+
+              ),
+            ),
+          ),
+
+          SizedBox.expand(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+              child: Center(
+                child: Container(
+                  padding: EdgeInsets.only(
+                    left: 30.w,
+                    right: 30.w,
+                  ),
+                  decoration: BoxDecoration(
+                    color: appPrimaryColour.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  width: 320.w,
+                  height: 490.h,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20.0),
+                        child: SingleChildScrollView(
+                          physics: const NeverScrollableScrollPhysics(),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              const Spacer(),
-                              Expanded(
-                                child: FractionallySizedBox(
-                                  widthFactor: 3,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        border: Border(
-                                            bottom: BorderSide(
-                                                color: appTertiaryColour, width: 1))),
-                                  ),
+                              SizedBox(height: 55.0.h),
+                              AvatarGlow(
+                                glowRadiusFactor: 0.2,
+                                glowCount: 2,
+                                glowColor: appSecondaryColour,
+                                child: Image.asset(
+                                  'assets/logo/applogo.png',
+                                  height: 80.0.h,
                                 ),
                               ),
-                              const Spacer(),
-                              Container(
-                                color: Colors.transparent,
-                                padding: const EdgeInsets.only(
-                                  left: 8.0,
-                                  right: 8.0,
-                                ),
-                                margin: const EdgeInsets.all(8.0),
-                                child: Text(
-                                  "SIGN IN",
-                                  style: boldTextStyle.copyWith(
-                                    fontWeight: FontWeight.w300,
-                                  ),
+                              SizedBox(height: 40.0.h),
+
+                              Center(
+                                child: Column(
+                                  children: [
+
+                                    Row(
+                                      children: [
+                                        const Spacer(),
+                                        Expanded(
+                                          child: FractionallySizedBox(
+                                            widthFactor: 3,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  border: Border(
+                                                      bottom: BorderSide(
+                                                          color: appQuarternaryColour, width: 2))),
+                                            ),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        Container(
+                                          color: Colors.transparent,
+                                          padding: const EdgeInsets.only(
+                                            left: 8.0,
+                                            right: 8.0,
+                                          ),
+                                          margin: const EdgeInsets.all(8.0),
+                                          child: Text(
+                                            "SIGN IN",
+                                            style: boldTextStyle.copyWith(
+                                              fontWeight: FontWeight.w600,
+                                            ),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                        Expanded(
+                                          child: FractionallySizedBox(
+                                            widthFactor: 3,
+                                            child: Container(
+                                              decoration: BoxDecoration(
+                                                  border: Border(
+                                                      bottom: BorderSide(
+                                                          color: appQuarternaryColour, width: 2))),
+                                            ),
+                                          ),
+                                        ),
+                                        const Spacer(),
+                                      ],
+                                    ),
+
+                                    SizedBox(height: 10.0.h),
+
+                                    SignInButton(
+                                      Buttons.email,
+                                      text: "Sign in with Email/Phone",
+                                      onPressed: () => Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => const LoginPage(),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ),
-                              const Spacer(),
-                              Expanded(
-                                child: FractionallySizedBox(
-                                  widthFactor: 3,
-                                  child: Container(
-                                    decoration: BoxDecoration(
-                                        border: Border(
-                                            bottom: BorderSide(
-                                                color: appTertiaryColour, width: 1))),
-                                  ),
-                                ),
+
+                              SignInButton(
+                                Buttons.google,
+                                text: "Sign in with Google",
+                                onPressed: () => signInWithGoogle(),
                               ),
-                              const Spacer(),
                             ],
                           ),
-
-                          const SizedBox(height: 10.0),
-
-                          SignInButton(
-                            Buttons.email,
-                            text: "Sign in with Email/Phone",
-                            onPressed: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const LoginPage(),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          Expanded(
+                            child: FractionallySizedBox(
+                              widthFactor: 3,
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: appQuarternaryColour, width: 2))),
                               ),
                             ),
                           ),
+                          const Spacer(),
+                          Container(
+                            color: Colors.transparent,
+                            padding: const EdgeInsets.only(
+                              left: 8.0,
+                              right: 8.0,
+                            ),
+                            margin: const EdgeInsets.all(8.0),
+                            child: Text(
+                              "SIGN UP",
+                              style: boldTextStyle.copyWith(
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
+                          Expanded(
+                            child: FractionallySizedBox(
+                              widthFactor: 3,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    border: Border(
+                                        bottom: BorderSide(
+                                            color: appQuarternaryColour, width: 2))),
+                              ),
+                            ),
+                          ),
+                          const Spacer(),
                         ],
                       ),
-                    ),
-
-                    SignInButton(
-                      Buttons.google,
-                      text: "Sign in with Google",
-                      onPressed: () => signInWithGoogle(),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Row(
-              children: [
-                const Spacer(),
-                Expanded(
-                  child: FractionallySizedBox(
-                    widthFactor: 3,
-                    child: Container(
-                      decoration: const BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: appTertiaryColour, width: 1))),
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                Container(
-                  color: Colors.transparent,
-                  padding: const EdgeInsets.only(
-                    left: 8.0,
-                    right: 8.0,
-                  ),
-                  margin: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "SIGN UP",
-                    style: boldTextStyle.copyWith(
-                      fontWeight: FontWeight.w300,
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                Expanded(
-                  child: FractionallySizedBox(
-                    widthFactor: 3,
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: appTertiaryColour, width: 1))),
-                    ),
-                  ),
-                ),
-                const Spacer(),
-              ],
-            ),
-            Padding( // This stays at the bottom
-              padding: const EdgeInsets.only(
-                top: 20.0,
-                bottom: 20.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text('Don\'t have an account?'),
-                  const SizedBox(width: 4.0),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignupPage(),
+                      Padding( // This stays at the bottom
+                        padding: EdgeInsets.only(
+                          top: 20.0,
+                          bottom: 20.0,
                         ),
-                      );
-                    },
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontWeight: FontWeight.w600,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Don\'t have an account?',
+                              style: boldTextStyle.copyWith(fontWeight: FontWeight.w500),
+                            ),
+                            const SizedBox(width: 4.0),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const SignupPage(),
+                                  ),
+                                );
+                              },
+                              child: const Text(
+                                'Sign Up',
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
+                      const Spacer(flex: 1),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-            const Spacer(flex: 1),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
