d59752afcc5a60f2382866238a5c7c74b9b5c6f4
Andrew Ellen <18004410@uhi.ac.uk>
2024-03-13 22:12:52 +0000

    Trying to get a curl request working

--- src/lib/pages/diet_new/diet_nutrition_table_extraction.dart
+++ src/lib/pages/diet_new/diet_nutrition_table_extraction.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 import 'dart:math';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:convert';
+import '../../OCRapiKey.dart';
 
 
 class NutritionTableExtraction extends StatefulWidget {
@@ -51,6 +53,44 @@ class _NutritionTableExtractionState extends State<NutritionTableExtraction> {
   }
 
 
+  Future<void> OCRTabularRecognition() async {
+
+
+    String base64Image = base64Encode(selectedImage!.readAsBytesSync());
+
+    final url = Uri.parse('https://api.ocr.space/parse/image');
+
+    final req = http.MultipartRequest('POST', url)
+      //..fields['base64Image'] = "data:image/jpg;base64," + base64Image
+      //..fields['filetype'] = "JPG"
+      ..fields['language'] = 'eng'
+      ..fields['isOverlayRequired'] = 'false'
+      ..fields['isTable'] = 'true'
+      ..fields['OCREngine'] = '2'
+      ..fields['scale'] = 'true'
+      ..fields['detectOrientation'] = 'true';
+
+    req.files.add(
+      await http.MultipartFile.fromPath(
+        File(selectedImage!.path).uri.pathSegments.last,
+        selectedImage!.path,
+      )
+    );
+    req.headers['apikey'] = OCRAPIKEY;
+
+    final stream = await req.send();
+    final res = await http.Response.fromStream(stream);
+    final status = res.statusCode;
+    if (status != 200) throw Exception('http.send error: statusCode= $status');
+
+
+    print("OCR CURL REQUEST BELOW");
+    print(res.body);
+
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -71,7 +111,7 @@ class _NutritionTableExtractionState extends State<NutritionTableExtraction> {
                       source: ImageSource.camera,
                       imageQuality: 100,
                     );
-          
+
                     //CroppedFile? croppedFile = await cropImage(image!);
           
                     setState(() {
@@ -79,6 +119,7 @@ class _NutritionTableExtractionState extends State<NutritionTableExtraction> {
                     });
 
                     await recogniseText();
+                    await OCRTabularRecognition();
 
                   } catch (error) {
                     debugPrint("Error Detected");
@@ -97,7 +138,7 @@ class _NutritionTableExtractionState extends State<NutritionTableExtraction> {
                     padding: const EdgeInsets.all(14.0),
                     child: Text(
                       recognizedText!.blocks[index].text,
-                      style: TextStyle(
+                      style: const TextStyle(
                         color: Colors.white,
                         fontSize: 20,
                       ),
