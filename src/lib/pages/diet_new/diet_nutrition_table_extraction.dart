88e95d4b2e5b75ab60a6fdf9d5667bfe638bfaab
Andrew Ellen <18004410@uhi.ac.uk>
2024-03-13 05:45:16 +0000

    Got text recognition and picture taking working

--- src/lib/pages/diet_new/diet_nutrition_table_extraction.dart
+++ src/lib/pages/diet_new/diet_nutrition_table_extraction.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class NutritionTableExtraction extends StatefulWidget {
@@ -9,11 +14,80 @@ class NutritionTableExtraction extends StatefulWidget {
 }
 
 class _NutritionTableExtractionState extends State<NutritionTableExtraction> {
+
+  File? selectedImage;
+  final textRecognizer = TextRecognizer(script: TextRecognitionScript.latin);
+
+  Future<void> recogniseText() async {
+    final RecognizedText recognizedText = await textRecognizer.processImage(
+        InputImage.fromFile(
+          selectedImage!,
+        )
+    );
+
+    String text = recognizedText.text;
+    for (TextBlock block in recognizedText.blocks) {
+      final Rect rect = block.boundingBox;
+      final List<Point<int>> cornerPoints = block.cornerPoints;
+      final String text = block.text;
+      final List<String> languages = block.recognizedLanguages;
+
+      print("Printing Block Text");
+      print(text);
+
+      for (TextLine line in block.lines) {
+        print("Printing Text Line");
+        print(line.text);
+        for (TextElement element in line.elements) {
+          print("Printing Text Element");
+          print(element.text);
+        }
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container(
 
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+          
+              selectedImage != null ? Image.file(selectedImage!) : const SizedBox.shrink(),
+          
+              ElevatedButton(
+                onPressed: () async {
+                  try {
+                    final XFile? image = await ImagePicker().pickImage(
+                      source: ImageSource.camera,
+                      imageQuality: 100,
+                    );
+          
+                    //CroppedFile? croppedFile = await cropImage(image!);
+          
+                    setState(() {
+                      selectedImage = File(image!.path);
+                    });
 
+                    await recogniseText();
+
+                  } catch (error) {
+                    debugPrint("Error Detected");
+                    debugPrint(error.toString());
+                  }
+                },
+                child: const Text("Pick From Camera"),
+              )
+          
+            ],
+          ),
+        ),
+      ),
 
     );
   }
