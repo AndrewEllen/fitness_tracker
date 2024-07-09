45d5fbec4dbe2a3fbefb43c11566341f015e5a3f
Andrew Ellen <18004410@uhi.ac.uk>
2024-07-10 00:00:34 +0100

    Added a better view for the training plan routines

--- src/lib/pages/workout_new/training_plan_page.dart
+++ src/lib/pages/workout_new/training_plan_page.dart
@@ -1,5 +1,6 @@
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:fitness_tracker/helpers/general/string_extensions.dart';
 import 'package:fitness_tracker/models/workout/training_plan_model.dart';
 import 'package:fitness_tracker/pages/workout_new/workout_log_page.dart';
 import 'package:fitness_tracker/pages/workout_new/workout_logs_home.dart';
@@ -16,7 +17,6 @@ import '../../widgets/general/app_default_button.dart';
 import '../../widgets/workout_new/home_page_routines_list.dart';
 import '../../widgets/workout_new/training_plan_day_box.dart';
 import 'exercise_database_search.dart';
-
 
 class TrainingPlanPage extends StatefulWidget {
   TrainingPlanPage({Key? key, required this.trainingPlan}) : super(key: key);
@@ -82,16 +82,16 @@ class _TrainingPlanPageState extends State<TrainingPlanPage> {
                       ),
                       enabledBorder: const OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: appQuarternaryColour,
-                          )),
+                        color: appQuarternaryColour,
+                      )),
                       focusedBorder: const OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: appSecondaryColour,
-                          )),
+                        color: appSecondaryColour,
+                      )),
                       border: const OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: appSecondaryColour,
-                          )),
+                        color: appSecondaryColour,
+                      )),
                     ),
                   ),
                 ),
@@ -147,226 +147,174 @@ class _TrainingPlanPageState extends State<TrainingPlanPage> {
   Widget build(BuildContext context) {
     //_key = GlobalKey<ExpandableFabState>();
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: appTertiaryColour,
-        title: Text(
-          widget.trainingPlan.trainingPlanName,
-          style: boldTextStyle,
-        ),
-      ),
-      resizeToAvoidBottomInset: false,
-      backgroundColor: appPrimaryColour,
-      floatingActionButtonLocation: ExpandableFab.location,
-      floatingActionButton: ExpandableFab(
-        key: _key,
-        distance: 80.w,
-        overlayStyle: ExpandableFabOverlayStyle(
-          blur: 2,
-        ),
-        openButtonBuilder: FloatingActionButtonBuilder(
-          size: 16.w,
-          builder: (BuildContext context, void Function()? onPressed,
-              Animation<double> progress) {
-            return AvatarGlow(
-              glowCount: context.watch<WorkoutProvider>().workoutStarted ? 3 : 0,
-              glowColor: Colors.red,
-              glowRadiusFactor: 0.3,
-              child: Material(
-                color: appSenaryColour,
-                elevation: 8.0,
-                shape: const CircleBorder(),
-                child: Padding(
-                  padding: EdgeInsets.all(16.w),
-                  child: const Icon(
-                    Icons.menu,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            );
-          },
-        ),
-        closeButtonBuilder: FloatingActionButtonBuilder(
-          size: 46.w,
-          builder: (BuildContext context, void Function()? onPressed,
-              Animation<double> progress) {
-            return FloatingActionButton(
-              foregroundColor: Colors.transparent,
-              backgroundColor: Colors.transparent,
-              heroTag: null,
-              child: Icon(
-                MdiIcons.closeCircle,
-                color: Colors.red,
-                size: 48.w,
-              ),
-              onPressed: onPressed,
-            );
-          },
-        ),
-        children: [
-          SizedBox(
-            width: 48.w,
-            child: FloatingActionButton(
-              tooltip: "Add Routine",
-              backgroundColor: appSecondaryColour,
-              heroTag: null,
-              child: const Icon(
-                Icons.add,
-              ),
-              onPressed: () => newRoutine(
-                this.context,
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 48.w,
-            child: FloatingActionButton(
-              tooltip: "View Past Workouts",
-              backgroundColor: appSecondaryColour,
-              heroTag: null,
-              child: const Icon(
-                MdiIcons.clipboardClock,
-              ),
-              onPressed: () {
-                final menuState = _key.currentState;
-                if (menuState != null) {
-                  menuState.toggle();
-                }
-                context
-                    .read<PageChange>()
-                    .changePageCache(WorkoutLogsHome());
-              },
-            ),
-          ),
-          SizedBox(
-            width: 48.w,
-            child: FloatingActionButton(
-              tooltip: "View Current Workout",
-              backgroundColor: context.watch<WorkoutProvider>().workoutStarted
-                  ? appSenaryColour
-                  : appSecondaryColour,
-              heroTag: null,
-              child: AvatarGlow(
-                glowCount:
-                context.watch<WorkoutProvider>().workoutStarted ? 3 : 0,
-                glowColor: Colors.red,
-                glowRadiusFactor: 0.7,
-                child: const Material(
-                  type: MaterialType.transparency,
-                  elevation: 8.0,
-                  shape: CircleBorder(),
-                  child: Icon(
-                    Icons.access_time,
-                  ),
-                ),
-              ),
-              onPressed: () {
-                final menuState = _key.currentState;
-                if (menuState != null) {
-                  menuState.toggle();
-                }
-                context.read<PageChange>().changePageCache(WorkoutLogPage());
-              },
-            ),
-          ),
-        ],
-      ),
-      body: NotificationListener<OverscrollIndicatorNotification>(
-        onNotification: (overscroll) {
-          overscroll.disallowIndicator();
-          return true;
-        },
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 14.h),
+    ///Currently focusing on one week only training plans. Index is here for future support
+    int index = 0;
 
-
-
-              ListView.builder(
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: widget.trainingPlan.trainingPlanWeeks.length,
-                itemBuilder: (BuildContext context, int index) {
-
-                  return Column(
-                    children: [
-
-                      Container(
-                        color: appTertiaryColour,
-                        width: double.maxFinite,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text(
-                            "Week ${index + 1}",
-                            style: boldTextStyle.copyWith(fontSize: 18.h),
+    return DefaultTabController(
+      length: 7,
+      initialIndex: DateTime.now().weekday-1,
+      child: PreferredSize(
+        preferredSize: Size.fromHeight(50.h),
+        child: Scaffold(
+          backgroundColor: appPrimaryColour,
+          appBar: AppBar(
+            actions: [
+              context.read<WorkoutProvider>().workoutStarted
+                  ? Padding(
+                      padding: EdgeInsets.only(top: 8.h, bottom: 8.h),
+                      child: FloatingActionButton(
+                        tooltip: "View Current Workout",
+                        backgroundColor: appSenaryColour,
+                        heroTag: null,
+                        child: AvatarGlow(
+                          glowCount: 3,
+                          glowColor: Colors.red,
+                          glowRadiusFactor: 0.7,
+                          child: const Material(
+                            type: MaterialType.transparency,
+                            elevation: 8.0,
+                            shape: CircleBorder(),
+                            child: Icon(
+                              Icons.access_time,
+                            ),
                           ),
                         ),
+                        onPressed: () {
+                          context
+                              .read<PageChange>()
+                              .changePageCache(WorkoutLogPage());
+                        },
                       ),
-
-                      TrainingPlanDayBox(
-                        day: "Monday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].mondayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].mondayRoutineID
-                        ) : -1,
-                      ),
-                      TrainingPlanDayBox(
-                        day: "Tuesday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].tuesdayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].tuesdayRoutineID
-                        ) : -1,
-                      ),
-                      TrainingPlanDayBox(
-                        day: "Wednesday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].wednesdayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].wednesdayRoutineID
-                        ) : -1,
-                      ),
-                      TrainingPlanDayBox(
-                        day: "Thursday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].thursdayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].thursdayRoutineID
-                        ) : -1,
-                      ),
-                      TrainingPlanDayBox(
-                        day: "Friday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].fridayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].fridayRoutineID
-                        ) : -1,
-                      ),
-                      TrainingPlanDayBox(
-                        day: "Saturday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].saturdayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].saturdayRoutineID
-                        ) : -1,
-                      ),
-                      TrainingPlanDayBox(
-                        day: "Sunday",
-                        routineIndex: widget.trainingPlan.trainingPlanWeeks[index].sundayRoutineID.isNotEmpty ? context.read<WorkoutProvider>().routinesList.indexWhere(
-                                (value) => value.routineID
-                                == widget.trainingPlan.trainingPlanWeeks[index].sundayRoutineID
-                        ) : -1,
-                      ),
-
-                    ],
-                  );
-
-                },
-              ),
-
-
+                    )
+                  : const SizedBox.shrink(),
             ],
+            backgroundColor: appTertiaryColour,
+            title: Text(
+              widget.trainingPlan.trainingPlanName.capitalize(),
+              style: boldTextStyle.copyWith(fontSize: 18),
+            ),
+            //elevation: 1.5,
+            //shadowColor: appSenaryColour,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(50.h),
+              child: Stack(
+                fit: StackFit.passthrough,
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Container(
+                    decoration: const BoxDecoration(
+                      border: Border(
+                        bottom:
+                            BorderSide(color: appSenaryColourDark, width: 2.0),
+                      ),
+                    ),
+                  ),
+                  const TabBar(
+                    isScrollable: true,
+                    indicatorColor: appSenaryColour,
+                    tabs: [
+                      Tab(text: "Monday"),
+                      Tab(text: "Tuesday"),
+                      Tab(text: "Wednesday"),
+                      Tab(text: "Thursday"),
+                      Tab(text: "Friday"),
+                      Tab(text: "Saturday"),
+                      Tab(text: "Sunday"),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          body: NotificationListener<OverscrollIndicatorNotification>(
+            onNotification: (overscroll) {
+              overscroll.disallowIndicator();
+              return true;
+            },
+            child: TabBarView(
+              children: [
+                TrainingPlanDayBox(
+                  day: "Monday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .mondayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .mondayRoutineID)
+                      : -1,
+                ),
+                TrainingPlanDayBox(
+                  day: "Tuesday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .tuesdayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .tuesdayRoutineID)
+                      : -1,
+                ),
+                TrainingPlanDayBox(
+                  day: "Wednesday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .wednesdayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .wednesdayRoutineID)
+                      : -1,
+                ),
+                TrainingPlanDayBox(
+                  day: "Thursday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .thursdayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .thursdayRoutineID)
+                      : -1,
+                ),
+                TrainingPlanDayBox(
+                  day: "Friday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .fridayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .fridayRoutineID)
+                      : -1,
+                ),
+                TrainingPlanDayBox(
+                  day: "Saturday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .saturdayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .saturdayRoutineID)
+                      : -1,
+                ),
+                TrainingPlanDayBox(
+                  day: "Sunday",
+                  routineIndex: widget.trainingPlan.trainingPlanWeeks[index]
+                          .sundayRoutineID.isNotEmpty
+                      ? context.read<WorkoutProvider>().routinesList.indexWhere(
+                          (value) =>
+                              value.routineID ==
+                              widget.trainingPlan.trainingPlanWeeks[index]
+                                  .sundayRoutineID)
+                      : -1,
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
